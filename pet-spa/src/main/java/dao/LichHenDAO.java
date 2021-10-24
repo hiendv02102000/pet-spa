6c24112acb5875733127c1b7f7934d514384e24a
Your Namedovanhien021000 <dovanhien021000@gmail.com>
2021-10-24 22:08:55 +0700

    xem hen

--- pet-spa/src/main/java/dao/LichHenDAO.java
+++ pet-spa/src/main/java/dao/LichHenDAO.java
@@ -4,10 +4,56 @@
  */
 package dao;
 
+import java.math.BigInteger;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.util.Vector;
+import model.DichVu;
+import model.LichHen;
+import utils.FormateDateTime;
+
 /**
  *
  * @author admin
  */
-public class LichHenDAO {
-    
+public class LichHenDAO extends DAO{
+    private final String ConditionsOfExist =  " ngayxoa IS  NULL";
+    public LichHenDAO() {
+        super();
+    }
+    public LichHen[] getByKhachHangID(int khID){
+         String sql = "Select * from tblLichHen Where tblKhachHangid = "+khID+" AND"+ConditionsOfExist;
+       ResultSet rs;
+       Vector<LichHen> listLH = new Vector<LichHen>();
+        LichHen[] result;
+        try{ 
+            Statement statement = this.conn.createStatement();
+            rs=statement.executeQuery(sql);
+            int count =0;
+           while(rs.next()){
+                LichHen lh = new LichHen(rs.getInt(1), 
+                        FormateDateTime.convertDBToLocalDateTime(rs.getDate(2), rs.getTime(2)), 
+                        new BigInteger(rs.getString(3)), 
+                        null, 
+                        null);
+             listLH.add(lh);
+             //return FormatVI.decodeVI(rs.getString(2));
+           }
+           
+           result = new LichHen[count];
+           
+          
+        }catch(Exception e){
+            return null;
+        }
+        return listLH.toArray(result);
+    }
+    public static void main(String[] args) {
+       LichHenDAO lhd = new LichHenDAO();
+       LichHen[] l = lhd.getByKhachHangID(1);
+       for(LichHen lh:l){
+           System.out.println(lh);
+       }
+    }
 }
